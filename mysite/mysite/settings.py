c0f24eb5dcea14391fa5d58d1f788e4550a41386
Phillip Tracy <tracyphill3@gmail.com>
2025-12-11 16:50:47 -0500

    Build out user registration and styling with crispy forms

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'register.apps.RegisterConfig',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK="bootstrap4"
+
